ee98d32e3537cff71132bf8329ce6649cdd89493
Rob-cop <31703520+Rob-cop@users.noreply.github.com>
2019-07-23 16:11:30 +0530

    initial commit

--- myExoplayer-master/myExoplayer/splashscreen/src/main/java/com/example/splashscreen/myAnimation.java
+++ myExoplayer-master/myExoplayer/splashscreen/src/main/java/com/example/splashscreen/myAnimation.java
@@ -18,14 +18,55 @@ public class myAnimation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_animation);
-        RelativeLayout layout=(RelativeLayout)findViewById(R.id.mainLayout);
+        final RelativeLayout layout=(RelativeLayout)findViewById(R.id.mainLayout);
         animation1= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animation1);
         animation1.setStartTime(50);
         layout.startAnimation(animation1);
-        ImageView view=(ImageView)findViewById(R.id.text);
-        animation2=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slidein);
-        animation2.setStartTime(125);
-        view.startAnimation(animation2);
+
+        animation1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                ImageView view=(ImageView)findViewById(R.id.text);
+                view.setVisibility(View.VISIBLE);
+                animation2=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slidein);
+                animation2.setStartTime(2000);
+                view.startAnimation(animation2);
+                animation2.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        ConstraintLayout parent=(ConstraintLayout)findViewById(R.id.parentLayout);
+
+                        animation3=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.lastpart);
+                        animation3.setStartTime(2150);
+                        parent.startAnimation(animation3);
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
 
 
     }
